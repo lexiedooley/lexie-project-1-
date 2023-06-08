f47356515b4ce678971ee7f5fa58ca04d5e6a747
lexiedooley <lexiedools@gmail.com>
2023-06-07 23:31:19 -0400

    updated JS a bit/struture, messing with randomizer

--- main.js
+++ main.js
@@ -1,71 +1,60 @@
-  /*----- constants -----*/
-const cards = document.querySelectorAll('.card-container')
-
-let flippedCard = false;
-let cardOne, cardTwo;
-let endGame = false;
-let restart
-  /*----- state variables -----*/
+const cards = document.querySelectorAll('div.card');
 
 
-  /*----- cached elements  -----*/
+let firstCard;
+let secondCard;
 
-
-  /*----- event listeners -----*/
-
-// for each card, listen for a click, if click happens run flippingCard
-cards.forEach(card => card.addEventListener('click', flippingCard));
-
-
-  /*----- functions -----*/
-
-// if card has been selected > check to see if the game is over > check to see if user is selecting cardOne. 
-//if both returned, flip the card. if the card has been flipped, return true and identify card0ne as flipped
-  function flippingCard() {
-    if (endGame) return;
-    if (this === cardOne) return;
-
-    this.classList.add('flip')
-
-    if (!flippedCard) {
-        flippedCard = true;
-        cardOne = this;
-    return;
-    }
-
-//if card two is equal to this invoke matchedCard
-cardTwo = this;
-matchedCard();
-
+// function to select card
+const selectCard = (card) => {
+// check if the card has been selected already
+  if (card.classList.contains('selected')) {
+ // if the card is selected, remove the class
+    card.classList.remove('selected');
+  }
+  else {
+ // if the card isn't selected, add the new class
+    card.classList.add('selected');
+  }
 }
-//let first and second cards strictly have to match
-function matchedCard() {
-    let match = cardOne.dataset.runes === cardTwo.dataset.runes;
-//if it is a match, don't flip the cards back over and lock them
-    if (match) {
-        lockCards();
-    }
-    else {
-        unflipCards();
-    }
-// make it so the cards don't flip back over if there is a match
-function lockCards() {
-    cardOne.removeEventListener('click', flippingCard);
-    cardTwo.removeEventListener('click', flippingCard);
-reset();
+// function to see if cards match
+const checkMatch = () => {
+// check to see if the data in runes matches
+  if (firstCard.dataset.runes === secondCard.dataset.runes) {
+    firstCard.classList.add('test');
+    secondCard.classList.add('test');
+    firstCard = undefined;
+    secondCard = undefined;
+  }
+  else {
+    firstCard.classList.remove('selected');
+    secondCard.classList.remove('selected');
+    firstCard = undefined;
+    secondCard = undefined;
+  }
 }
-//if the game ends, unflip the cards
- function unflipCards() {
-    endGame = true;
-        cardOne.classList.remove('flip')
-        cardTwo.classList.remove('flip')
-        reset();
- }
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].id = `card-${i}`;
+  cards[i].addEventListener("click", () => {
+    const card = document.getElementById(`card-${i}`);
+    
+    // check whether or not we have a card selected yet
+    if (firstCard === undefined && secondCard === undefined) {
+      selectCard(card);
+// assign the value of firstCard (originally undefined) to be the card that was clicked
+      firstCard = card;
     }
-
-// restart = document.getElementById('restart');
-// restart.addEventListener("click", restart);
-
-// function fullRestart() {
-
-// }
+    
+// if we have 1 card selected and we do not select the same card as our second card
+    else if (firstCard !== undefined && secondCard === undefined && firstCard !== card) {
+// invoke the function select card passing in the SECOND card value
+      selectCard(card);
+// assign the value of secondCard to be the third div
+      secondCard = card;
+// invoke checkMatch
+      checkMatch();
+    }
+  });
+}
+//shuffle the cards
+//reset game
